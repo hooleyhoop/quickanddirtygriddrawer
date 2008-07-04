c92422c23744e36667769d06414aad44154537a8
Steve Hooley <steven.hooley@bestbefore.tv>
2008-07-04 16:20:59 +0100

    first commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  gridDrawer
+//
+//  Created by steve hooley on 04/07/2008.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
